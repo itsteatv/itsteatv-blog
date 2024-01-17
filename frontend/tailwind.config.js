f8325d21859bf7474f109d10727dbaa39ee95ed4
itsteatv <rezashow60@gmail.com>
2024-01-17 03:52:05 +0330

    style: Add custom font and refine Tailwind CSS configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 const { nextui } = require("@nextui-org/react");
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      screens: {
-        ">=400px": { "min": "9.375em", "max": "25em" },
-      },
+content: [
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+  "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+],
+theme: {
+  extend: {
+    screens: {
+      ">=400px": { "min": "9.375em", "max": "25em" },
     },
+    fontFamily: {
+      "SourceCodePro": ['Source Code Pro', 'monospace']
+    }
   },
-  darkMode: "class",
-  plugins: [nextui()],
+},
+darkMode: "class",
+plugins: [nextui()],
 }
