9e81e9ed98ddb02a83bce6fd6edd4d8d70e9c905
itsteatv <rezashow60@gmail.com>
2024-01-21 21:57:15 +0330

    feat: Enhance user signup endpoint with JWT token and data formatting

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import express from "express"
 import mongoose from "mongoose"
 import "dotenv/config"
+import bcrypt from "bcrypt"
 import { emailRegex, passwordRegex } from "./regexPatterns.js";
+import jwt from "jsonwebtoken"
+
+import User from "./Schema/User.js"
+import { nanoid } from "nanoid";
 
 const server = express()
 const PORT = process.env.PORT || 3000
@@ -20,9 +25,32 @@ const connectDB = async () => {
 
 connectDB()
 
+const formatDataToSend = function (user) {
+
+    const access_token = jwt.sign({ id: user._id }, process.env.SECRET_ACCESS_KEY)
+
+    return {
+        access_token,
+        profile_img: user.personal_info.profile_img,
+        username: user.personal_info.username,
+        fullname: user.personal_info.fullname
+    }
+}
+
+const generateUsername = async function (email) {
+    const username = email.split("@")[0]
+    const usernameExists = await User.exists({ "personal_info.username": username }).then((result) => result)
+
+    if (usernameExists) {
+        return username + nanoid()
+    }
+
+
+    return username
+}
 
 server.post("/signup", (req, res) => {
-    let { fullname, email, password } = req.body;
+    const { fullname, email, password } = req.body;
 
     if (!fullname || fullname.length < 3) {
         return res.status(403).json({ error: "Fullname must be at least 3 characters long" });
@@ -39,6 +67,32 @@ server.post("/signup", (req, res) => {
     if (!passwordRegex.test(password)) {
         return res.status(403).json({ error: "Invalid password. Password must be 6 to 20 characters long and include at least one digit, one lowercase letter, and one uppercase letter." });
     }
+
+    bcrypt.hash(password, 10, async (error, hashed_password) => {
+        if (error) {
+            return res.status(500).json({ "error": error.message });
+        }
+
+        const username = await generateUsername(email);
+
+        const user = new User({
+            "personal_info": { fullname, email, password: hashed_password, username }
+        });
+
+        user.save().then((u) => {
+            return res.status(200).json(formatDataToSend(u))
+        }).catch((error) => {
+            if (error.code === 11000) {
+                return res.status(500).json({ "error": "Email already exists" })
+            }
+
+            return res.status(500).json({ "error": error.message })
+        })
+
+        console.log(hashed_password);
+    })
+
+
 })
 
 server.listen(PORT, () => {
