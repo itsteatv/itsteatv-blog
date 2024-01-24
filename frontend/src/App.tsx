046d04d45045521c458b9a60365a1859030fa147
itsteatv <rezashow60@gmail.com>
2024-01-24 05:41:07 +0330

    feat: Integrate react query & react hot toast in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,34 @@
 import Navbar from "./ui/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserAuthForm from "./ui/UserAuthForm";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 0,
+        refetchOnReconnect: true,
+        refetchOnWindowFocus: true,
+      },
+    },
+  });
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navbar />}>
-          <Route path="signup" element={<UserAuthForm type="sign-up" />} />
-          <Route path="signin" element={<UserAuthForm type="sign-in" />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />}>
+            <Route path="signup" element={<UserAuthForm type="sign-up" />} />
+            <Route path="signin" element={<UserAuthForm type="sign-in" />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <Toaster />
+    </QueryClientProvider>
   );
 }
 
