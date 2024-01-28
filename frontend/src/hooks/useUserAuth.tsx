b28a545690d33e264b110dd8c01139e5e3cc67d2
itsteatv <rezashow60@gmail.com>
2024-01-28 06:24:01 +0330

    refactor: Replaced js-cookie library with react-cookie in useUserAuth hook

--- frontend/src/hooks/useUserAuth.tsx
+++ frontend/src/hooks/useUserAuth.tsx
@@ -2,9 +2,12 @@ import { useMutation } from "@tanstack/react-query";
 import { UserAuthAPI } from "../api/UserAuthAPI";
 import { UserAuthAPIData } from "../utils/types";
 import toast from "react-hot-toast";
-import Cookies from "js-cookie";
+// import Cookies from "js-cookie";
+import { useCookies } from "react-cookie";
 
 export function useUserAuth() {
+  const [cookies, setCookies] = useCookies(["access_token"]);
+
   const { isPending, mutate: userAuth } = useMutation({
     mutationFn: (payload: UserAuthAPIData) => UserAuthAPI(payload),
 
@@ -16,7 +19,11 @@ export function useUserAuth() {
         toast.success("Successfully signed in!");
 
         const access_token = data.access_token;
-        Cookies.set("access_token", access_token);
+        setCookies("access_token", access_token, {
+          path: "/",
+          maxAge: 15 * 24 * 60 * 60 * 1000,
+          secure: true,
+        });
       }
     },
 
