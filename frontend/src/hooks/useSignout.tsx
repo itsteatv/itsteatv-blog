eef7d99c9488f3b84e2e03cea7b8f5d6dae6d920
itsteatv <rezashow60@gmail.com>
2024-02-08 14:37:17 +0330

    refactor: Update useSignout hook to use localStorage instead of Cookies for removing access token

--- frontend/src/hooks/useSignout.tsx
+++ frontend/src/hooks/useSignout.tsx
@@ -1,17 +1,15 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import Cookies from "js-cookie";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 export function useSignout() {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
-  const token = Cookies.get("access_token");
-  console.log(token);
 
   const { mutate: signout, isPending } = useMutation({
-    mutationFn: () => Cookies.remove("access_token"),
-
+    mutationFn: async () => {
+      localStorage.removeItem("access_token");
+    },
     onSuccess: () => {
       queryClient.removeQueries();
       toast.success("You've been signed out successfully"),
