25bca76c879d5ae634dc309dbb802335803e033c
itsteatv <rezashow60@gmail.com>
2024-02-08 14:32:45 +0330

    refactor: Update useUserData hook to use localStorage for access token retrieval

--- frontend/src/hooks/useUserData.tsx
+++ frontend/src/hooks/useUserData.tsx
@@ -1,9 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
-import Cookies from "js-cookie";
 import { UserDataAPI } from "../api/UserDataAPI";
 
 export function useUserData() {
-  const token = Cookies.get("access_token");
+  const token = localStorage.getItem("access_token");
   console.log(token);
 
   const { isLoading, data: user } = useQuery({
