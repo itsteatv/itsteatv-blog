9271c7018bcb70b9f03cf1d365e192f5a1ad6249
itsteatv <rezashow60@gmail.com>
2024-02-13 21:36:12 +0330

    feat: Implement conditional rendering based on authentication status in Editor component

--- frontend/src/pages/Editor.tsx
+++ frontend/src/pages/Editor.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
 function Editor() {
+  const [editorState, setEditorState] = useState("editor");
+
+  const token = localStorage.getItem("access_token");
+
   return (
     <div className="flex items-center justify-center min-h-screen font-SometypeMono font-bold">
-      Editor Page
+      {token === null ? (
+        <Navigate replace to="signin" />
+      ) : (
+        <h1>{editorState === "editor" ? "Blog Editor" : "Publish Form"}</h1>
+      )}
     </div>
   );
 }
