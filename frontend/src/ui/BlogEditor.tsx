aab16752b42f916c2c6e144d6ce9c4f934c75bd8
itsteatv <rezashow60@gmail.com>
2024-02-15 14:01:37 +0330

    feat(ui): Update BlogEditor component with navigation and buttons

--- frontend/src/ui/BlogEditor.tsx
+++ frontend/src/ui/BlogEditor.tsx
@@ -1,5 +1,41 @@
+import { Link } from "react-router-dom";
+import ItsteatvLogo from "./ItsteatvLogo";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Button,
+} from "@nextui-org/react";
+
 function BlogEditor() {
-  return <div className="flex items-center font-bold font-SometypeMono min-h-screen">Blog Editor</div>;
+  return (
+    <div className="flex items-center font-bold font-SometypeMono">
+      <Navbar>
+        <NavbarBrand>
+          <Link to="..">
+            <ItsteatvLogo />
+          </Link>
+          <p className="font-bold text-inherit hidden sm:flex">New Blog</p>
+        </NavbarBrand>
+        <NavbarContent justify="end">
+          <NavbarItem className="flex gap-2">
+            <Button
+              color="success"
+              variant="solid"
+              radius="full"
+              className="text-white"
+            >
+              Publish
+            </Button>
+            <Button color="default" variant="light" radius="full">
+              Save Draft
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
+    </div>
+  );
 }
 
 export default BlogEditor;
