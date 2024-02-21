cecd7c1e67b3bb4dd7d5a48753c942d28fe3a0da
itsteatv <rezashow60@gmail.com>
2024-02-21 06:27:53 +0330

    feat: Introduce EditorContext and update Editor component

--- frontend/src/pages/Editor.tsx
+++ frontend/src/pages/Editor.tsx
@@ -1,21 +1,38 @@
 import { useState } from "react";
 import { Navigate } from "react-router-dom";
+import { createContext } from "react";
 import BlogEditor from "../ui/BlogEditor";
 import PublishForm from "../ui/PublishForm";
 
+const blogPost = {
+  title: "",
+  banner: "",
+  content: [],
+  tags: [],
+  desc: "",
+  author: { personal_info: {} },
+};
+
+export const EditorContext = createContext({});
+
 function Editor() {
+  const [blog, setBlog] = useState(blogPost);
   const [editorState, setEditorState] = useState("editor");
 
   const token = localStorage.getItem("access_token");
 
   return (
-    <div>
-      {token === null ? (
-        <Navigate replace to="signin" />
-      ) : (
-        <h1>{editorState === "editor" ? <BlogEditor /> : <PublishForm />}</h1>
-      )}
-    </div>
+    <EditorContext.Provider
+      value={{ blog, setBlog, editorState, setEditorState }}
+    >
+      <div>
+        {token === null ? (
+          <Navigate replace to="signin" />
+        ) : (
+          <h1>{editorState === "editor" ? <BlogEditor /> : <PublishForm />}</h1>
+        )}
+      </div>
+    </EditorContext.Provider>
   );
 }
 
