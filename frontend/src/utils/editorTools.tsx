39054741c61bff9b0422f73fd37d5de27a5b6dd2
itsteatv <rezashow60@gmail.com>
2024-02-25 19:39:30 +0330

    feat(editor): Integrate Editor.js tools for rich text editing

---
+++ frontend/src/utils/editorTools.tsx
@@ -0,0 +1,21 @@
+import Checklist from "@editorjs/checklist";
+import Embed from "@editorjs/embed";
+import Header from "@editorjs/header";
+import Image from "@editorjs/image";
+import Link from "@editorjs/link";
+import List from "@editorjs/list";
+import Quote from "@editorjs/quote";
+import Raw from "@editorjs/raw";
+import SimpleImage from "@editorjs/simple-image";
+
+export const editorTools = {
+  checkList: Checklist,
+  embed: Embed,
+  header: Header,
+  image: Image,
+  link: Link,
+  list: List,
+  quote: Quote,
+  raw: Raw,
+  simpleImage: SimpleImage,
+};
