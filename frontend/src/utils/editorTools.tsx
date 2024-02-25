d52bc03e95af3645e61dc1fb03a31a51fb11c019
itsteatv <rezashow60@gmail.com>
2024-02-25 20:58:52 +0330

    chore: Add @ts-ignore to suppress TypeScript errors for editor tool imports

--- frontend/src/utils/editorTools.tsx
+++ frontend/src/utils/editorTools.tsx
@@ -1,11 +1,20 @@
+// @ts-ignore
 import Checklist from "@editorjs/checklist";
+// @ts-ignore
 import Embed from "@editorjs/embed";
+// @ts-ignore
 import Header from "@editorjs/header";
+// @ts-ignore
 import Image from "@editorjs/image";
+// @ts-ignore
 import Link from "@editorjs/link";
+// @ts-ignore
 import List from "@editorjs/list";
+// @ts-ignore
 import Quote from "@editorjs/quote";
+// @ts-ignore
 import Raw from "@editorjs/raw";
+// @ts-ignore
 import SimpleImage from "@editorjs/simple-image";
 
 export const editorTools = {
