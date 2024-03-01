75d1005a025b338b7a1061ac5400a5f6a36cf463
itsteatv <rezashow60@gmail.com>
2024-03-01 21:31:06 +0330

    feat(publish): Integrate close functionality and context usage in PublishForm component

--- frontend/src/ui/PublishForm.tsx
+++ frontend/src/ui/PublishForm.tsx
@@ -1,7 +1,27 @@
+import { useContext } from "react";
+import { EditorContext } from "../pages/Editor";
+import { GoX } from "react-icons/go";
+
 function PublishForm() {
+  const { blog, setBlog, textEditor, setTextEditor, setEditorState } =
+    useContext(EditorContext);
+  const { title, banner, content, tags, desc } = blog;
+
+  const handleCloseEvent = function () {
+    setEditorState("editor");
+  };
+
+  console.log(title, banner, content, tags, desc);
+
   return (
-    <div className="flex items-center font-bold font-SometypeMono min-h-screen">
-      Publish Form
+    <div className="">
+      <div className="flex items-end justify-end">
+        <GoX
+          onClick={handleCloseEvent}
+          className="mt-4 mr-4 cursor-pointer"
+          size={30}
+        />
+      </div>
     </div>
   );
 }
