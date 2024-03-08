60d084afced0b6fbad8a2810b66a6703cec62efd
itsteatv <rezashow60@gmail.com>
2024-03-08 04:01:30 +0330

    feat: Enhance Tag component with editing functionality

--- frontend/src/ui/Tag.tsx
+++ frontend/src/ui/Tag.tsx
@@ -1,12 +1,40 @@
-import { Chip } from "@nextui-org/react";
+import { Chip, Input } from "@nextui-org/react";
+import { useState } from "react";
 
-function Tag({ tag }) {
+function Tag({ tag }: { tag: string }, key: number) {
+  console.log(tag);
+
+  const [editing, setEditing] = useState(false);
+  const [editedTag, setEditedTag] = useState(tag);
+
+  const handleEdit = () => {
+    setEditing(true);
+  };
+
+  const handleTagChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEditedTag(e.target.value);
+  };
+
+  const handleSave = () => {
+    setEditing(false);
+  };
+
   return (
     <div className="flex gap-4">
-      <Chip onClose={() => console.log("close")}>Chip</Chip>
-      <Chip onClose={() => console.log("close")} variant="bordered">
-        {tag}
-      </Chip>
+      {editing ? (
+        <Input
+          variant="underlined"
+          value={editedTag}
+          onChange={handleTagChange}
+          onBlur={handleSave}
+          autoFocus
+          size="sm"
+        />
+      ) : (
+        <Chip key={key} onClick={handleEdit} variant="bordered">
+          {editedTag}
+        </Chip>
+      )}
     </div>
   );
 }
